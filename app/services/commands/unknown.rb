b6d8a75399657420d6b19f0c979d58bba2afbfbe
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2019-07-20 23:02:27 -0700

    Add more phrases for unknown response

--- app/services/commands/unknown.rb
+++ app/services/commands/unknown.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 class Commands::Unknown
   PHRASES = [
+    "*I don't know* but I think I'll go and try dog mushing with an eskimo",
+    "*I don't know* what changed my mind, 'til then I was the ramblin' kind",
     "Don't ask me cuz *I don't know*; I just fasten my seat belt wherever I go",
-    "*I can't talk* my talk with you"
+    "Don't want to be anything where *I don't know* when to stop",
+    "If I could I would, but *I don't know* how",
+    "Trapped in time and *I don't know* what to do"
   ].freeze
 
   def call
