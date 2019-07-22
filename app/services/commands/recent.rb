8bc424fcf3a5c39dca74f5186928b87a66c90f8e
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2019-07-21 20:57:59 -0700

    Cleanup

--- app/services/commands/recent.rb
+++ app/services/commands/recent.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+
+# TODO: Write a spec but ait until `first`, `shortest`, `longest`, etc
+# are investigated as they will require much of the functionality here
 class Commands::Recent
   include CommandsHelper
 
@@ -11,35 +14,35 @@ class Commands::Recent
   end
 
   def call
-    return latest_show_setlist if option.blank?
+    return last_show_setlist if option.blank?
     track_details || DEFAULT_RESPONSE
   end
 
   private
 
-  def latest_show_setlist
-    ::Commands::Date.new(date: latest_show.date).call
+  def last_show_setlist
+    ::Commands::Date.new(date: last_show.date).call
   end
 
-  def latest_show
-    @latest_show ||=
+  def last_show
+    @last_show ||=
       Phishin::Client.call(
         'shows',
         params: { sort_attr: :date, sort_dir: :desc, per_page: 1 }
       ).first
   end
 
-  def latest_track
+  def last_track
     track_by_song_slug || track_by_title_search
   end
 
   def track_details
-    return if latest_track.nil?
-    date = latest_track.show_date
+    return if last_track.nil?
+    date = last_track.show_date
     show = show_on_date(date)
-    str = "The last performance of *#{latest_track.title}* was on "
+    str = "The last performance of *#{last_track.title}* was on "
     str += "*#{pretty_date(date)}* @ *#{show.venue_name}*\n"
-    str + "https://phish.in/#{date}/#{latest_track.slug}"
+    str + "https://phish.in/#{date}/#{last_track.slug}"
   end
 
   def show_on_date(date)
