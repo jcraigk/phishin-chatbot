610618227e6ff58e881ddf513053e7465c6a6faf
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2019-07-22 21:35:26 -0700

    Add random command, refactor recent

--- app/helpers/commands_helper.rb
+++ app/helpers/commands_helper.rb
@@ -21,11 +21,42 @@ module CommandsHelper
     SET_NAMES[set] || 'Unknown Set'
   end
 
-  def duration_readable(milliseconds, style: 'colons')
+  def readable_duration(milliseconds, style: 'colons')
     DurationFormatter.new(milliseconds, style).call
   end
 
   def pretty_date(date)
     Date.parse(date).strftime('%B %e, %Y')
+  end
+
+  def show_on_date(date)
+    Phishin::Client.call("shows/#{date}")
+  end
+
+  def song_match(str)
+    song_by_slug(str) || song_by_partial_title(str)
+  end
+
+  def song_by_slug(slug)
+    Phishin::Client.call("songs/#{slug}")
+  end
+
+  def song_by_partial_title(term)
+    song_by_slug(first_matching_song(term).slug)
+  end
+
+  def first_matching_song(term)
+    Phishin::Client.call("search/#{term}").songs.first
+  end
+
+  # TODO: add tags
+  def track_details_from_collection(tracks, idx)
+    track = tracks[idx]
+    date = track.show_date
+    show = show_on_date(date)
+    str = "*#{pretty_date(date)}* @ *#{show.venue_name}* clocking in at "
+    str += "*#{readable_duration(track.duration)}*.  "
+    str += "It's the *#{(idx + 1).ordinalize}* of *#{tracks.size}* total performances.\n"
+    str + "https://phish.in/#{date}/#{track.slug}"
   end
 end
