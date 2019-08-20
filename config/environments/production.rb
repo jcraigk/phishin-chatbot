46156bfe64ba7db90eb95b7d1d2949ecd6ebb9ce
Justin Craig-Kuhn (JCK) <jcraigk@gmail.com>
2019-08-19 22:10:22 -0700

    Remove chatbot.phish.in host from production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,4 @@ Rails.application.configure do
   logger = ActiveSupport::Logger.new(STDOUT)
   logger.formatter = config.log_formatter
   config.logger = ActiveSupport::TaggedLogging.new(logger)
-
-  config.hosts << 'chatbot.phish.in'
 end
